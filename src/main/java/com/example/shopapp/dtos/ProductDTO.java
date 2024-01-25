a3f181c4089c13325919c4770e284dd59ab276f2
trungnhandev <trungnhanforwork@gmail.com>
2024-01-25 17:56:42 +0700

    Update adding image feature

--- src/main/java/com/example/shopapp/dtos/ProductDTO.java
+++ src/main/java/com/example/shopapp/dtos/ProductDTO.java
@@ -6,6 +6,9 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Data
 @Getter
@@ -25,4 +28,6 @@ public class ProductDTO {
 
     @JsonProperty("category_id")
     private String categoryId;
+
+    private List<MultipartFile> files;
 }
