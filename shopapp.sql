b8b96c3037b1608b757ae05338d7dc17550ad124
trungnhan <trungnhanforwork@gmail.com>
2024-01-25 23:26:30 +0700

    chore(database.sql): table product image is added.

--- shopapp.sql
+++ shopapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 17, 2024 at 04:47 PM
+-- Generation Time: Jan 25, 2024 at 05:25 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL DEFAULT '' COMMENT 'Category name'
+                              `id` int(11) NOT NULL,
+                              `name` varchar(100) NOT NULL DEFAULT '' COMMENT 'Category name'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -39,22 +39,22 @@ CREATE TABLE `categories` (
 --
 
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `fullname` varchar(100) DEFAULT '',
-  `email` varchar(100) DEFAULT '',
-  `phone_number` varchar(20) NOT NULL,
-  `address` varchar(200) NOT NULL,
-  `note` varchar(100) DEFAULT '',
-  `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` enum('pending','processing','shipped','delivered','cancelled') DEFAULT NULL,
-  `total_money` float DEFAULT NULL CHECK (`total_money` >= 0),
-  `shipping_method` varchar(100) DEFAULT NULL,
-  `shipping_address` varchar(200) DEFAULT NULL,
-  `shipping_date` date DEFAULT NULL,
-  `tracking_number` varchar(100) DEFAULT NULL,
-  `payment_method` varchar(100) DEFAULT NULL,
-  `active` tinyint(1) DEFAULT NULL
+                          `id` int(11) NOT NULL,
+                          `user_id` int(11) DEFAULT NULL,
+                          `fullname` varchar(100) DEFAULT '',
+                          `email` varchar(100) DEFAULT '',
+                          `phone_number` varchar(20) NOT NULL,
+                          `address` varchar(200) NOT NULL,
+                          `note` varchar(100) DEFAULT '',
+                          `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
+                          `status` enum('pending','processing','shipped','delivered','cancelled') DEFAULT NULL,
+                          `total_money` float DEFAULT NULL CHECK (`total_money` >= 0),
+                          `shipping_method` varchar(100) DEFAULT NULL,
+                          `shipping_address` varchar(200) DEFAULT NULL,
+                          `shipping_date` date DEFAULT NULL,
+                          `tracking_number` varchar(100) DEFAULT NULL,
+                          `payment_method` varchar(100) DEFAULT NULL,
+                          `active` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -64,13 +64,13 @@ CREATE TABLE `orders` (
 --
 
 CREATE TABLE `order_details` (
-  `id` int(11) NOT NULL,
-  `order_id` int(11) DEFAULT NULL,
-  `product_id` int(11) DEFAULT NULL,
-  `price` float DEFAULT NULL CHECK (`price` >= 0),
-  `number_of_products` int(11) DEFAULT NULL CHECK (`number_of_products` > 0),
-  `total_money` float DEFAULT NULL CHECK (`total_money` >= 0),
-  `type` varchar(20) DEFAULT '' COMMENT 'Type of product'
+                                 `id` int(11) NOT NULL,
+                                 `order_id` int(11) DEFAULT NULL,
+                                 `product_id` int(11) DEFAULT NULL,
+                                 `price` float DEFAULT NULL CHECK (`price` >= 0),
+                                 `number_of_products` int(11) DEFAULT NULL CHECK (`number_of_products` > 0),
+                                 `total_money` float DEFAULT NULL CHECK (`total_money` >= 0),
+                                 `type` varchar(20) DEFAULT '' COMMENT 'Type of product'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -80,14 +80,26 @@ CREATE TABLE `order_details` (
 --
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `name` varchar(350) DEFAULT NULL COMMENT 'Tên sản phẩm',
-  `price` float NOT NULL CHECK (`price` >= 0),
-  `thumbnail` varchar(300) DEFAULT '',
-  `description` longtext DEFAULT '',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `category_id` int(11) DEFAULT NULL
+                            `id` int(11) NOT NULL,
+                            `name` varchar(350) DEFAULT NULL COMMENT 'Tên sản phẩm',
+                            `price` float NOT NULL CHECK (`price` >= 0),
+                            `thumbnail` varchar(300) DEFAULT '',
+                            `description` longtext DEFAULT '',
+                            `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+                            `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+                            `category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_images`
+--
+
+CREATE TABLE `product_images` (
+                                  `id` int(11) NOT NULL,
+                                  `product_id` int(11) DEFAULT NULL,
+                                  `image_url` varchar(300) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -97,8 +109,8 @@ CREATE TABLE `products` (
 --
 
 CREATE TABLE `roles` (
-  `id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
+                         `id` int(11) NOT NULL,
+                         `name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -108,12 +120,12 @@ CREATE TABLE `roles` (
 --
 
 CREATE TABLE `social_account` (
-  `id` int(11) NOT NULL,
-  `provider` varchar(20) NOT NULL COMMENT 'Name of provider',
-  `provider_id` varchar(50) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `user_id` int(11) DEFAULT NULL
+                                  `id` int(11) NOT NULL,
+                                  `provider` varchar(20) NOT NULL COMMENT 'Name of provider',
+                                  `provider_id` varchar(50) NOT NULL,
+                                  `email` varchar(150) NOT NULL,
+                                  `name` varchar(100) NOT NULL,
+                                  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -123,13 +135,13 @@ CREATE TABLE `social_account` (
 --
 
 CREATE TABLE `tokens` (
-  `id` int(11) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `token_type` varchar(50) NOT NULL,
-  `expiration_date` datetime DEFAULT NULL,
-  `revoked` tinyint(1) NOT NULL,
-  `expired` tinyint(1) NOT NULL,
-  `user_id` int(11) DEFAULT NULL
+                          `id` int(11) NOT NULL,
+                          `token` varchar(255) NOT NULL,
+                          `token_type` varchar(50) NOT NULL,
+                          `expiration_date` datetime DEFAULT NULL,
+                          `revoked` tinyint(1) NOT NULL,
+                          `expired` tinyint(1) NOT NULL,
+                          `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -139,18 +151,18 @@ CREATE TABLE `tokens` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `fullname` varchar(100) DEFAULT '',
-  `phone_number` varchar(10) NOT NULL,
-  `address` varchar(200) DEFAULT '',
-  `password` varchar(100) NOT NULL DEFAULT '',
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `is_active` tinyint(4) DEFAULT 1,
-  `date_of_birth` date DEFAULT NULL,
-  `facebook_account_id` int(11) DEFAULT 0,
-  `google_account_id` int(11) DEFAULT 0,
-  `role_id` int(11) DEFAULT NULL
+                         `id` int(11) NOT NULL,
+                         `fullname` varchar(100) DEFAULT '',
+                         `phone_number` varchar(10) NOT NULL,
+                         `address` varchar(200) DEFAULT '',
+                         `password` varchar(100) NOT NULL DEFAULT '',
+                         `created_at` datetime DEFAULT NULL,
+                         `updated_at` datetime DEFAULT NULL,
+                         `is_active` tinyint(4) DEFAULT 1,
+                         `date_of_birth` date DEFAULT NULL,
+                         `facebook_account_id` int(11) DEFAULT 0,
+                         `google_account_id` int(11) DEFAULT 0,
+                         `role_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -161,20 +173,20 @@ CREATE TABLE `users` (
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `order_details`
 --
 ALTER TABLE `order_details`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD KEY `order_id` (`order_id`),
   ADD KEY `product_id` (`product_id`);
 
@@ -182,27 +194,34 @@ ALTER TABLE `order_details`
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `product_images`
+--
+ALTER TABLE `product_images`
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `roles`
 --
 ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `social_account`
 --
 ALTER TABLE `social_account`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `tokens`
 --
 ALTER TABLE `tokens`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `token` (`token`),
   ADD KEY `user_id` (`user_id`);
 
@@ -210,7 +229,7 @@ ALTER TABLE `tokens`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD KEY `role_id` (`role_id`);
 
 --
@@ -221,43 +240,49 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product_images`
+--
+ALTER TABLE `product_images`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `social_account`
 --
 ALTER TABLE `social_account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tokens`
 --
 ALTER TABLE `tokens`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -267,38 +292,44 @@ ALTER TABLE `users`
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+    ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `order_details`
 --
 ALTER TABLE `order_details`
-  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+    ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
   ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
 --
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+    ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `product_images`
+--
+ALTER TABLE `product_images`
+    ADD CONSTRAINT `product_images_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `social_account`
 --
 ALTER TABLE `social_account`
-  ADD CONSTRAINT `social_account_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+    ADD CONSTRAINT `social_account_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `tokens`
 --
 ALTER TABLE `tokens`
-  ADD CONSTRAINT `tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+    ADD CONSTRAINT `tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+    ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
